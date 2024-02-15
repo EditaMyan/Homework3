364c8a1cd1d9f37e4458bc14c6e955abf7560719
user <user@WIN-LNN38JED6LU>
2024-02-15 20:01:14 +0400

    Add project files.

---
+++ Homework3/Category.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public sealed class Category
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        private static List<Category> predefinedCategories = new List<Category>()
+        {
+        new Category("Fiction", "Books that describe imaginary events or people."),
+        new Category("Non-Fiction", "Books that are based on facts and real events."),
+        new Category("Science Fiction", "Books that feature futuristic science and technology."),
+        new Category("Mystery", "Books that involve solving a crime or puzzle."),
+        new Category("Romance", "Books that focus on romantic love stories."),
+        new Category("Fantasy", "Books that contain magic, mythical creatures, or supernatural elements.")
+        };
+
+        public Category(string categoryName, string description)
+        {
+            CategoryName = categoryName;
+            Description = description;
+        }
+
+        public static void ListPredefinedCategories()
+        {
+            Console.WriteLine("Predefined Categories:");
+            foreach (var category in predefinedCategories)
+            {
+                Console.WriteLine($"{category.CategoryName}: {category.Description}");
+            }
+        }
+
+        public static Category GetCategoryByName(string categoryName)
+        {
+            return predefinedCategories.Find(c => c.CategoryName.Equals(categoryName, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ Homework3/LibraryManagement.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public static class LibraryManagment
+    {
+        private static List<Book> books = new List<Book>();
+
+        public static void Run()
+        {
+            while (true)
+            {
+                OfferingChoice();
+            }
+        }
+
+        private static void OfferingChoice()
+        {
+            
+            Console.WriteLine("1. Add Book");
+            Console.WriteLine("2. Remove Book");
+            Console.WriteLine("3. List all Books");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select an option: ");
+
+            int choice = Convert.ToInt32(Console.ReadLine()); 
+
+            switch (choice)
+            {
+                case 1:
+                    AddBook();
+                    break;
+                case 2:
+                    RemoveBook();
+                    break;
+                case 3:
+                    ListAllBooks();
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+        public static void AddBook()
+        {
+            Console.WriteLine("Enter Book Details:");
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Author Name: ");
+            string authorName = Console.ReadLine();
+
+            Console.Write("Author Biography: ");
+            string authorBio = Console.ReadLine();
+
+            Category.ListPredefinedCategories();
+            Console.Write("Category Name: ");
+            string categoryName = Console.ReadLine();
+
+            Category selectedCategory = Category.GetCategoryByName(categoryName);
+            if (selectedCategory == null)
+            {
+                Console.WriteLine("Invalid category.");
+                return;
+            }
+
+            Console.Write("Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Price: ");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+
+            Author author = new Author(authorName, authorBio);
+            //Category category = new Category(categoryName, categoryDesc);
+            Book book = new Book(title, author, selectedCategory, year, price);
+
+            books.Add(book);
+            Console.WriteLine("Book added succesfully.");
+        }
+
+        public static void RemoveBook()
+        {
+            Console.Write("Enter the title of the book to remove: ");
+            string title = Console.ReadLine();
+            Book bookToRemove = books.Find(book => book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+                Console.WriteLine("Book removed successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Book not found!");
+            }
+        }
+
+        public static void ListAllBooks()
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books in the library.");
+            }
+            else
+            {
+                Console.WriteLine("List of Books:");
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"Title: {book.Title}, Author: {book.Author.Name}, Category: {book.Category.CategoryName}, Year: {book.Year}, Price: {book.Price}");
+                }
+            }
+        }
+    }
+
+
+}
